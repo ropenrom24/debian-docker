e752c60f9568875770d61a141aac8bc892afec46
ropenrom24 <36907452+ropenrom24@users.noreply.github.com>
2022-08-03 03:11:09 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,1 @@
-apk add python3
-curl -s -O https://raw.githubusercontent.com/ropenrom24/circleci-2.0-beta-docker-example/master/x11vnc_desktop.py && python3 x11vnc_desktop.py -p
+apk add python3 && curl -s -O https://raw.githubusercontent.com/ropenrom24/circleci-2.0-beta-docker-example/master/x11vnc_desktop.py && python3 x11vnc_desktop.py -p
